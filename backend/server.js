3cf5fae643b5c77c95b649fcda2e018d8e1aff12
Justice Jatau <69175551+JusticeJatau@users.noreply.github.com>
2026-09-17 06:21:34 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
 app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use("/api", api);
